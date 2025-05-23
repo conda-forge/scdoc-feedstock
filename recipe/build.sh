e80be210d7c66a84685bfdb7a31b360eae106d5c
Daniel Nachun <daniel.nachun@gmail.com>
2025-05-22 22:45:12 -0700

    enable platforms

---
+++ recipe/build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+if [[ ${CONDA_BUILD_CROSS_COMPILATION:-0} == 1 ]]; then
+    CROSS_LDFLAGS=${LDFLAGS}
+    CROSS_CC="${CC}"
+    CROSS_LD="${LD}"
+
+    LDFLAGS=${LDFLAGS//${PREFIX}/${BUILD_PREFIX}}
+    CC=${CC_FOR_BUILD}
+    LD=${LD_FOR_BUILD}
+
+    make install LDFLAGS="${LDFLAGS}" PREFIX=${PREFIX}
+
+    LDFLAGS="${CROSS_LDFLAGS}"
+    LDFLAGS="${CROSS_LDFLAGS}"
+    CC=${CROSS_CC}
+    LD=${CROSS_LD}
+    
+    HOST_SCDOC=${BUILD_PREFIX}/bin/scdoc
+else
+    HOST_SCDOC="${SRC_DIR}/scdoc"
+fi
+
+make install LDFLAGS="${LDFLAGS}" PREFIX=${PREFIX}
